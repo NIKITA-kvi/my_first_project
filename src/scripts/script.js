44e4fb73e133ab29b63d322129e152e3bc7eed91
NIKITA KVINTA <nikitakvinta@gmail.com>
2025-03-28 09:03:41 +0100

    enroll-form-fix

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,15 +1,3 @@
-// 1 шаг тут мне нужно получить элементы со страницы из DOM дерева и поместить их в переменную
+const heading = document.querySelector('.heading');
 
-const slider = document.querySelector('.teachers-slider');
-const scrollBar = document.querySelector('.teachers-controls__scrollbar');
-const thumb = document.querySelector('.teachers-controls__scrollbar-progress');
-const prevBtn = document.querySelector('.teachers-slider__button--prev');
-const nextBtn = document.querySelector('.teachers-slider__button--next');
-
-// Шаг 2 тут я хочу удостоверится или правильно нашел элементы из DOM дерева
-
-console.log(slider);
-console.log(scrollBar);
-console.log(thumb);
-console.log(prevBtn);
-console.log(nextBtn);
+console.log(heading.innerHTML); // eslint-disable-line no-console
