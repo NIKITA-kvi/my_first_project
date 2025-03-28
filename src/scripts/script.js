5632e18d183d9955d54799d10acd65afb8e256ec
NIKITA KVINTA <nikitakvinta@gmail.com>
2025-03-28 09:00:32 +0100

    enroll-form

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,15 @@
-const heading = document.querySelector('.heading');
+// 1 шаг тут мне нужно получить элементы со страницы из DOM дерева и поместить их в переменную
 
-console.log(heading.innerHTML); // eslint-disable-line no-console
+const slider = document.querySelector('.teachers-slider');
+const scrollBar = document.querySelector('.teachers-controls__scrollbar');
+const thumb = document.querySelector('.teachers-controls__scrollbar-progress');
+const prevBtn = document.querySelector('.teachers-slider__button--prev');
+const nextBtn = document.querySelector('.teachers-slider__button--next');
+
+// Шаг 2 тут я хочу удостоверится или правильно нашел элементы из DOM дерева
+
+console.log(slider);
+console.log(scrollBar);
+console.log(thumb);
+console.log(prevBtn);
+console.log(nextBtn);
