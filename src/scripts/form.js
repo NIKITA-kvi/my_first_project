1bc8ec01806caba260226fabfbd2be3a82f91f91
NIKITAe <nikitakvinta@gmail.com>
2025-05-03 22:38:58 +0200

    formUpdate

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -15,40 +15,35 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 function highlightInput(input, isValid) {
   if (isValid) {
-    input.classList.remove('enroll-field__input--invalid'); // Убираем красную границу, если данные корректны
-  } else {
-    input.classList.add('enroll-field__input--invalid'); // Добавляем красную границу при ошибке
+    input.classList.remove('enroll-field__input--invalid');
+    return;
   }
+  input.classList.add('enroll-field__input--invalid');
 }
 
 nameInput.addEventListener('input', (event) => {
   formState.name = event.target.value;
-
   if (formState.name === '') {
     nameInput.setCustomValidity('Поле "Имя" обязательно для заполнения.');
     highlightInput(nameInput, false);
     return;
   }
-
   if (!nameRegex.test(formState.name)) {
     nameInput.setCustomValidity('Имя должно содержать только русские буквы и пробелы.');
     highlightInput(nameInput, false);
     return;
   }
-
   nameInput.setCustomValidity('');
   highlightInput(nameInput, true);
 });
 
 phoneInput.addEventListener('input', (event) => {
   formState.phone = event.target.value;
-
   if (formState.phone === '') {
     phoneInput.setCustomValidity('Поле "Телефон" обязательно для заполнения.');
     highlightInput(phoneInput, false);
     return;
   }
-
   if (!phoneRegex.test(formState.phone)) {
     phoneInput.setCustomValidity('Телефон должен содержать только цифры.');
     highlightInput(phoneInput, false);
@@ -60,19 +55,16 @@ phoneInput.addEventListener('input', (event) => {
 
 emailInput.addEventListener('input', (event) => {
   formState.email = event.target.value;
-
   if (formState.email === '') {
     emailInput.setCustomValidity('Поле "Email" обязательно для заполнения.');
     highlightInput(emailInput, false);
     return;
   }
-
   if (!emailRegex.test(formState.email)) {
     emailInput.setCustomValidity('Введите корректный email (например, test@mail.ru).');
     highlightInput(emailInput, false);
     return;
   }
-
   emailInput.setCustomValidity('');
   highlightInput(emailInput, true);
 });
